3257089b95a73284a78e7a28b07cff0fdc944c87
shenfive@gmail.com <Danny Shen>
2022-11-19 15:54:03 +0800

    map

--- A209HelloARKit/ViewController.swift
+++ A209HelloARKit/ViewController.swift
@@ -31,16 +31,34 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.debugOptions = [.showWorldOrigin,.showFeaturePoints]
         
         
+//
+//        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01) //新增一個 BOX
+//        let material = SCNMaterial() //新增材質
+//        material.diffuse.contents = UIColor.red  //材質內容為紅色
+//        box.materials = [material] //把 box 的貼圖材質加進去
+//        let node = SCNNode(geometry: box) //新增一個 Box
+//        node.position = SCNVector3(0, 0, -0.5) //設定 node 在空間的位置
+//
+//
+//        sceneView.scene.rootNode.addChildNode(node) //把 node 加入到目前的 scene 上
         
-        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01) //新增一個 BOX
-        let material = SCNMaterial() //新增材質
-        material.diffuse.contents = UIColor.red  //材質內容為紅色
-        box.materials = [material] //把 box 的貼圖材質加進去
-        let node = SCNNode(geometry: box) //新增一個 Box
-        node.position = SCNVector3(0, 0, -0.5) //設定 node 在空間的位置
+        
+        let text = SCNText(string: "Hello 申潤五", extrusionDepth: 1.0)
+        text.firstMaterial?.diffuse.contents = UIColor.blue
+        
+        let textNode = SCNNode(geometry: text)
+        textNode.position = SCNVector3(-0.3, 0.05, -0.5)
+        textNode.scale = SCNVector3(0.01, 0.01, 0.01)
+        
+        sceneView.scene.rootNode.addChildNode(textNode)
         
         
-        sceneView.scene.rootNode.addChildNode(node) //把 node 加入到目前的 scene 上
+        
+        let earth = SCNSphere(radius: 0.3)
+        earth.firstMaterial?.diffuse.contents = UIImage(named: "worldmap")
+        let earthNode = SCNNode(geometry: earth)
+        earthNode.position = SCNVector3(0, 0, -1)
+        sceneView.scene.rootNode.addChildNode(earthNode)
         
     }
     
