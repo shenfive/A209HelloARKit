199aae5b0d9f749345b0e3af3e07cbc1460e1d77
shenfive@gmail.com <Danny Shen>
2022-11-19 16:58:47 +0800

    update Plane

--- A209HelloARKit/ViewController.swift
+++ A209HelloARKit/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var planes:[OverlayPlane] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,25 +25,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
         let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
         sceneView.debugOptions = [.showWorldOrigin,.showFeaturePoints]
-        
-
-//
-//        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01) //新增一個 BOX
-//        let material = SCNMaterial() //新增材質
-//        material.diffuse.contents = UIColor.red  //材質內容為紅色
-//        box.materials = [material] //把 box 的貼圖材質加進去
-//        let node = SCNNode(geometry: box) //新增一個 Box
-//        node.position = SCNVector3(0, 0, -0.5) //設定 node 在空間的位置
-//
-//
-//        sceneView.scene.rootNode.addChildNode(node) //把 node 加入到目前的 scene 上
-        
+            
         
         let text = SCNText(string: "Hello 申潤五", extrusionDepth: 1.0)
         text.firstMaterial?.diffuse.contents = UIColor.blue
@@ -66,7 +55,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-        
         configuration.planeDetection = [.horizontal]
         
         // Run the view's session
@@ -74,10 +62,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-    }
+
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
@@ -95,41 +80,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                      blue:  CGFloat(arc4random()) / CGFloat(UInt32.max),
                      alpha: 1.0)
                  hitResult[0].node.geometry?.materials[0].diffuse.contents = randomColor
-         }else{
-             print("nothing!")
          }
      }
 
     
-    
-    
-    // MARK: - ARSCNViewDelegate
-    
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         print("find Plane")
+        if !(anchor is ARPlaneAnchor) { return } //確定找到加入的是 plane
+        let plane = OverlayPlane(anchor: anchor as! ARPlaneAnchor) //產出自訂義的可視平台
+        self.planes.append(plane) //新增到 ViewController 的記錄中
+        node.addChildNode(plane) //把自訂義的可視元件，蓋一層到平台上
     }
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        if let plane = self.planes.filter { plane in
+            return plane.anchor.identifier == anchor.identifier
+        }.first{
+            plane.update(anchor: anchor as! ARPlaneAnchor)
+        }
     }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+
+
+
 }
